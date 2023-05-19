1598fcb598ff112e38067314610a534086c4ac61
bjharding <benjamin.j.harding@gmail.com>
2023-05-20 09:21:34 +1000

    added envrc

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
-{ pkgs ? import <nixpkgs> { } }:
-
+# Shell for bootstrapping flake-enabled nix and home-manager
+{ pkgs ? let
+  # If pkgs is not defined, instantiate nixpkgs from locked commit
+  lock =
+    (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+  nixpkgs = fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+    sha256 = lock.narHash;
+  };
+  system = builtins.currentSystem;
+  overlays = [ ]; # Explicit blank overlay to avoid interference
+in import nixpkgs { inherit system overlays; }, ... }:
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    rustc
-    cargo
-    rustfmt
-    rust-analyzer
-    clippy
-    postgresql
-  ];
-
-  RUST_BACKTRACE = 1;
+  # Enable experimental features without having to specify the argument
+  NIX_CONFIG = "experimental-features = nix-command flakes";
+  nativeBuildInputs = with pkgs; [ nix home-manager git nixfmt ];
 }
