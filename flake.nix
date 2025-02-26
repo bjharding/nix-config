29299b790cbf66623c0ff5c69e77030ddab047f3
bjharding <benjamin.j.harding@gmail.com>
2025-02-27 08:38:46 +1100

    Removed some comments

--- flake.nix
+++ flake.nix
@@ -9,17 +9,6 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # agenix = {
-    #   url = "github:ryantm/agenix";
-    #   inputs = {
-    #     nixpkgs.follows = "nixpkgs";
-    #     home-manager.follows = "home-manager";
-    #   };
-    # };
-    # deploy-rs = {
-    #   url = "github:serokell/deploy-rs";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
     neovim-plugins = {
       url = "github:LongerHV/neovim-plugins-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -32,10 +21,6 @@
       url = "github:LongerHV/kubectl-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # nixgl = {
-    #   url = "github:guibou/nixGL";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
   outputs =
@@ -44,12 +29,9 @@
     , nixpkgs-unstable
     , nixos-hardware
     , home-manager
-      # , agenix
-      # , deploy-rs
     , neovim-plugins
     , xenon
     , kubectl
-      # , nixgl
     , ...
     }@inputs:
     let
@@ -59,8 +41,6 @@
       overlays = [
         neovim-plugins.overlays.default
         kubectl.overlays.default
-        # agenix.overlays.default
-        # nixgl.overlays.default
         (final: prev: {
           unstable = nixpkgs-unstable.legacyPackages.${prev.system};
           inherit (nixpkgs-unstable.legacyPackages.${prev.system}) neovim-unwrapped;
@@ -96,7 +76,6 @@
       nixosConfigurations =
         let
           defaultModules = (builtins.attrValues nixosModules) ++ [
-            # agenix.nixosModules.default
             home-manager.nixosModules.default
           ];
           specialArgs = { inherit inputs outputs overlays; };
@@ -115,52 +94,6 @@
               ./nixos/work
             ];
           };
-
-          #   nasgul = nixpkgs.lib.nixosSystem {
-          #     inherit specialArgs;
-          #     modules = defaultModules ++ [
-          #       ./nixos/nasgul
-          #     ];
-          #   };
-          #   playground = nixpkgs.lib.nixosSystem {
-          #     inherit specialArgs;
-          #     modules = defaultModules ++ [
-          #       ./nixos/playground
-          #     ];
-          #   };
-          #   smaug = nixpkgs.lib.nixosSystem {
-          #     inherit specialArgs;
-          #     modules = defaultModules ++ [
-          #       ./nixos/smaug
-          #     ];
-          #   };
-          #   sd-image = nixpkgs.lib.nixosSystem {
-          #     inherit specialArgs;
-          #     modules = defaultModules ++ [
-          #       "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-          #       ./nixos/sd-image
-          #     ];
-          #   };
-          #   isoimage = nixpkgs.lib.nixosSystem {
-          #     system = "x86_64-linux";
-          #     inherit specialArgs;
-          #     modules = defaultModules ++ [
-          #       "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
-          #       { isoImage.squashfsCompression = "gzip -Xcompression-level 1"; }
-          #       ./nixos/iso
-          #     ];
-          #   };
-          #   isoimage-server = nixpkgs.lib.nixosSystem {
-          #     system = "x86_64-linux";
-          #     inherit specialArgs;
-          #     modules = defaultModules ++ [
-          #       "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          #       {
-          #         isoImage.squashfsCompression = "gzip -Xcompression-level 1";
-          #         mySystem.user = "nixos";
-          #       }
-          #     ];
-          #   };
         };
 
       homeConfigurations = {
@@ -180,30 +113,5 @@
           ];
         };
       };
-
-      #   deploy.nodes =
-      #     let
-      #       mkDeployConfig = hostname: configuration: {
-      #         inherit hostname;
-      #         profiles.system =
-      #           let
-      #             inherit (configuration.config.nixpkgs.hostPlatform) system;
-      #           in
-      #           {
-      #             path = deploy-rs.lib."${system}".activate.nixos configuration;
-      #             sshUser = "longer";
-      #             user = "root";
-      #             sshOpts = [ "-t" ];
-      #             magicRollback = false; # Disable because it breaks remote sudo :<
-      #             interactiveSudo = true;
-      #           };
-      #       };
-      #     in
-      #     {
-      #       nasgul = mkDeployConfig "nasgul.lan" self.nixosConfigurations.nasgul;
-      #       smaug = mkDeployConfig "smaug.lan" self.nixosConfigurations.smaug;
-      #     };
-
-      #   checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
 }
